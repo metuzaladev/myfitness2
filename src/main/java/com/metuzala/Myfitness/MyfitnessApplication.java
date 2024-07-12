987587898c7f832201d321859756b4eeef482102
kriz:metz <koskor44@gmail.com>
2024-07-12 13:15:19 +0300

    idk first commit

---
+++ src/main/java/com/metuzala/Myfitness/MyfitnessApplication.java
@@ -0,0 +1,13 @@
+package com.metuzala.Myfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metuzala/Myfitness/MyfitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metuzala.Myfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
